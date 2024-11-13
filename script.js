80ce920dc4a8aeb6a2443e4fc6c04dbdf1c54bbe
ScarletHaeven <ramzyedeling5@gmail.com>
2024-11-13 19:27:15 +0100

    Minor script changes

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const views = ['intro', 'programming', 'games', 'music'];
 
 function openPageSection(id) {
   views.forEach((view) => {
-    let viewElement = document.getElementById(view.toLowerCase())
+    let viewElement = document.getElementById(view.toLowerCase());
     viewElement.classList.toggle('hidden', view !== id);
   });
 }
-
