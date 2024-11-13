d3c5770560faf034aa88580c25f84af5bc5b62e8
ScarletHaeven <ramzyedeling5@gmail.com>
2024-11-13 19:26:54 +0100

    Updated site

---
+++ script.js
@@ -0,0 +1,9 @@
+const views = ['intro', 'programming', 'games', 'music'];
+
+function openPageSection(id) {
+  views.forEach((view) => {
+    let viewElement = document.getElementById(view.toLowerCase())
+    viewElement.classList.toggle('hidden', view !== id);
+  });
+}
+
